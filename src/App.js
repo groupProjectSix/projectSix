0889eea9603d44a5df5bdb7a80c56eb0d0585ab2
Bethan <bethancdavies@gmail.com>
2019-12-04 17:42:16 -0500

    updates to QueryWord

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { Component } from 'react';
-import dbRef from './firebase'
-import './App.scss';
+import React, { Component } from "react";
+// import dbRef from "./firebase";
+import "./App.scss";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import QueryWord from "./QueryWord"
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      wordArray: []
-    }
+      // wordArray: [],
+      userInput: ""
+    };
+  }
+
+  saveUserInput = event => {
+    this.setState({
+      userInput: event.target.value
+    });
+    console.log(this.state.userInput);
+  }; 
+   
+  preventDefaultFunction= event =>{
+    event.preventDefault();
   }
 
   render() {
     return (
-      <React.Fragment>
-        <label htmlFor="wordInput">Enter a Word!</label>
-        <input type="text" id="wordInput" />
-        {/* setup onClick function for button */}
-        <button>Generate Backronym!</button>
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <form className="saveInput">
+            <label htmlFor="wordInput">Enter a Word!</label>
+            <input type="text" id="wordInput" onChange={this.saveUserInput} />
+            {/* setup onClick function for button */}
+            
+          <button onClick={this.preventDefaultFunction} type="submit">
+            <Link to="search">
+             Generate Backronym!
+            </Link> 
+          </button>
+
+          </form> 
+          <Route exact path="/search" render={()=>{return <QueryWord userInputProp={this.state.userInput} /> }} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
