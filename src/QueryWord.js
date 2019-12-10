500f3f2d01f4704cbcd10a3043faa3e1180c68c9
lynseyod <lyns.odonnell@gmail.com>
2019-12-09 20:26:35 -0500

    order is an illusion :sparkles::star::boom:

--- src/QueryWord.js
+++ src/QueryWord.js
@@ -30,106 +30,114 @@ class QueryWord extends Component {
       url:`https://api.datamuse.com/words`, 
       method: "get", 
       params:{ 
-          ml:`${userWord}`,
-          sp: `${firstLetter}*`,
-          md: "p"
-        }
-      }).then((data)=>{
-        console.log(data)
-        const arrayOfLetterObject = data.data;
-        this.setState({
-          firstWordArray: arrayOfLetterObject,
-        })
-      }).then( () => {
-        let randomWordNumber = this.generateRandomNumber(this.state.firstWordArray);
-        const firstWord = (this.state.firstWordArray[randomWordNumber].word);
-        const arrayForFirstWord = [];
-        arrayForFirstWord.push(firstWord)
-        this.setState({
-          firstSelectedWord: firstWord,
-          finalWord: arrayForFirstWord
-        });
-      }).then(() => {
-        let lastLetter = false;
-        for (let i = 1; i <= this.props.spreadLettersProp.length - 1; i++) {
-          if (i === this.props.spreadLettersProp.length - 1) {
-            lastLetter = true;
-          }
-          this.callToApiSecond(this.props.spreadLettersProp[i], this.state.finalWord[i], lastLetter);
-        }
+        ml:`${userWord}`,
+        sp: `${firstLetter}*`,
+        md: "p"
+      }
+    }).then((data)=>{
+      console.log(data)
+      const arrayOfLetterObject = data.data;
+      this.setState({
+        firstWordArray: arrayOfLetterObject,
       })
+    }).then( () => {
+      let randomWordNumber = this.generateRandomNumber(this.state.firstWordArray);
+      const firstWord = (this.state.firstWordArray[randomWordNumber].word);
+      const arrayForFirstWord = [];
+      arrayForFirstWord.push(firstWord)
+      this.setState({
+        firstSelectedWord: firstWord,
+        finalWord: arrayForFirstWord
+      });
+    }).then( async () => {
+      let lastLetter = false;
+      for (let i = 1; i <= this.props.spreadLettersProp.length - 1; i++) {
+        if (i === this.props.spreadLettersProp.length - 1) {
+          lastLetter = true;
+        }
+        const newWords = this.callToApiSecond(this.props.spreadLettersProp[i], this.state.finalWord[i]);
+        console.log(newWords);
+        // if (newWords.data.length < 2) {
+        //   this.randomWordApiCall(this.props.spreadLettersProp[i], lastLetter);
+        // } else {
+        //   this.handleApiData(newWords, lastLetter);
+        // }
+      }
+    })
   }
 
-  callToApiSecond = (nextLetter, prevWord, isItWordFinal) => {
-      axios ({
-        url: `https://api.datamuse.com/words`,
-        method: "get",
-        params: {
-          lc: `${prevWord}`,
-          sp: `${nextLetter}*`,
-          md: "p"
-        }
-      }).then((data) => {
-        if (data.data.length < 2) {
-          this.randomWordApiCall(nextLetter, isItWordFinal);
-        } else {
-          this.handleApiData(data, isItWordFinal);
-        }
-      })
-    }
+  callToApiSecond = (nextLetter, prevWord) => {
+    axios ({
+      url: `https://api.datamuse.com/words`,
+      method: "get",
+      params: {
+        lc: `${prevWord}`,
+        sp: `${nextLetter}*`,
+        md: "p"
+      }
+    })// .then((data) => {
+      // console.log(data);
+      // return data
+      // if (data.data.length < 2) {
+      //   this.randomWordApiCall(nextLetter, isItWordFinal);
+      // } else {
+      //   this.handleApiData(data, isItWordFinal);
+      // }
+    // })
+  }
 
-    randomWordApiCall = (nextLetter, isItWordFinal) => {
-      axios ({
-        url: `https://api.datamuse.com/words`,
-        method: "get",
-        params: {
-          sp: `${nextLetter}*`,
-          max: 100,
-          md: "p"
-        }
-      }).then((data) => {
-        this.handleApiData(data, isItWordFinal);
-      })
-    }
+  randomWordApiCall = (nextLetter, isItWordFinal) => {
+    axios ({
+      url: `https://api.datamuse.com/words`,
+      method: "get",
+      params: {
+        sp: `${nextLetter}*`,
+        max: 100,
+        md: "p"
+      }
+    }).then((data) => {
+      this.handleApiData(data, isItWordFinal);
+    })
+  }
 
-    handleApiData = (data, isItWordFinal) => {
-      console.log(data);
-        const newWordArray = [];
-        data.data.map((wordObject) => {
-          newWordArray.push(wordObject)
-        })
-        const ongoingWordArray = [...this.state.restOfWordsArray];
-        ongoingWordArray.push(newWordArray)
-        this.setState({
-          restOfWordsArray: ongoingWordArray
-        });
-        let finalWord = [];
-        let isItANoun = false;
-        let actualStringToPush = "";
-        const fillerWord = ["of", "and"];
-        const randomFillerWord = this.generateRandomNumber(fillerWord);
-        console.log(fillerWord, randomFillerWord)
-        finalWord = [...this.state.finalWord];
-        const wordToPush = newWordArray[this.generateRandomNumber(newWordArray)]
-        if (isItWordFinal) { //check if it's word final letter to avoid 'of'
-          finalWord.push(newWordArray[this.generateRandomNumber(newWordArray)].word)
-        } else { //otherwise throw 'of' on the end!
-          for (let i = 0; i <= wordToPush.tags.length; i++) {
-            if (wordToPush.tags[i] === "n") {
-              isItANoun = true;
-            }
-          }
-          if (isItANoun) {
-            actualStringToPush = `${wordToPush.word} ${fillerWord[randomFillerWord]}`
-          } else {
-            actualStringToPush = `${wordToPush.word}`
-          }
-          finalWord.push(actualStringToPush)
+  handleApiData = (data, isItWordFinal) => {
+    console.log(data);
+    const newWordArray = [];
+    data.data.map((wordObject) => {
+      newWordArray.push(wordObject)
+    })
+    const ongoingWordArray = [...this.state.restOfWordsArray];
+    ongoingWordArray.push(newWordArray)
+    this.setState({
+      restOfWordsArray: ongoingWordArray
+    });
+    let finalWord = [];
+    let isItANoun = false;
+    let actualStringToPush = "";
+    const fillerWord = ["of", "and"];
+    const randomFillerWord = this.generateRandomNumber(fillerWord);
+    console.log(fillerWord, randomFillerWord)
+    finalWord = [...this.state.finalWord];
+    const wordToPush = newWordArray[this.generateRandomNumber(newWordArray)]
+    if (isItWordFinal) { //check if it's word final letter to avoid 'of'
+      finalWord.push(newWordArray[this.generateRandomNumber(newWordArray)].word)
+    } else { //otherwise throw 'of' on the end!
+      for (let i = 0; i <= wordToPush.tags.length; i++) {
+        if (wordToPush.tags[i] === "n") {
+          isItANoun = true;
         }
-        this.setState({
-          finalWord: finalWord,
-        });
+      }
+      if (isItANoun) {
+        actualStringToPush = `${wordToPush.word} ${fillerWord[randomFillerWord]}`
+      } else {
+        actualStringToPush = `${wordToPush.word}`
+      }
+      finalWord.push(actualStringToPush)
     }
+    this.setState({
+      finalWord: finalWord,
+    });
+  }
 
   componentDidMount() {
     this.callToApiFirst(this.props.userInputProp, this.props.spreadLettersProp[0]);
