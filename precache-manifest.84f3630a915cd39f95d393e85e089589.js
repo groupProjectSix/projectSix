dc506785c2ea608a3d15178d69d8cbdfa45d587d
lynseyod <lyns.odonnell@gmail.com>
2019-12-10 17:36:03 -0500

    Updates

---
+++ precache-manifest.84f3630a915cd39f95d393e85e089589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e826cfc2784d445e6a8e5c8a48a35691",
+    "url": "/projectSix/index.html"
+  },
+  {
+    "revision": "4bbe00f90e769551918c",
+    "url": "/projectSix/static/css/main.de84bdfa.chunk.css"
+  },
+  {
+    "revision": "d62932139c5f9ca8603c",
+    "url": "/projectSix/static/js/2.421fa0f7.chunk.js"
+  },
+  {
+    "revision": "77d00c363a65bdc3b9f679a811dd2019",
+    "url": "/projectSix/static/js/2.421fa0f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bbe00f90e769551918c",
+    "url": "/projectSix/static/js/main.05b23c09.chunk.js"
+  },
+  {
+    "revision": "55134e920b2bf9a3eb90",
+    "url": "/projectSix/static/js/runtime-main.4bba6cf4.js"
+  },
+  {
+    "revision": "d65b87efb6303aa1c5f89eabfaa1f55a",
+    "url": "/projectSix/static/media/logo.d65b87ef.svg"
+  }
+]);
