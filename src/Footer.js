8bda4c28ec7fec4422e56c5f0ab21a96ad383cfd
Bethan <bethancdavies@gmail.com>
2019-12-10 16:47:15 -0500

    added my name :(

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,12 @@ class Footer extends Component{
     render(){
         return(
             <footer>
-                <p> © Backronym Generator created by: </p>
-                <p>  
+                <p> © Backronym Generator created by: <br></br>
+                
                     <a target="_blank" href={'http://sandy.codes'}>Sandy Pranjic</a>
                     <a target="_blank" href={'http://www.lynsey.codes'}>Lynsey O'Donnell</a>
                     <a target="_blank" href={'http://wadebutler.ca/'}>Wade Butler</a>
-                    <a target="_blank" href={'http://www.bethancdavies.com'}>Bethan</a>
+                    <a target="_blank" href={'http://www.bethancdavies.com'}>Bethan Davies</a>
                  </p>
                 
                 
